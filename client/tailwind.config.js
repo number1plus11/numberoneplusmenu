688e8bc100cbae6f1b88d8c65dad5a0bec63b10c
rayen kalai <im.rayen.k@gmail.com>
2026-02-16 14:57:53 +0100

    Update theme color to strict Red #ed2121

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,17 +8,17 @@ export default {
         extend: {
             colors: {
                 orange: {
-                    50: '#fff1f2',  // Very light red/rose
-                    100: '#ffe4e6', // light red
-                    200: '#fecdd3',
-                    300: '#fda4af',
-                    400: '#fb7185',
-                    500: '#f43f5e',
-                    600: '#ed2121', // Primary brand color requested
-                    700: '#be123c',
-                    800: '#9f1239',
-                    900: '#881337',
-                    950: '#4c0519',
+                    50: '#fef2f2',
+                    100: '#fee2e2',
+                    200: '#fecaca',
+                    300: '#fca5a5',
+                    400: '#f87171',
+                    500: '#ed2121', // Setting 500 to the requested color
+                    600: '#ed2121', // Setting 600 to the requested color as well to ensure coverage
+                    700: '#b91c1c',
+                    800: '#991b1b',
+                    900: '#7f1d1d',
+                    950: '#450a0a',
                 },
             },
         },
